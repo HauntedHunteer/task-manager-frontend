84d8f97c865dd6b75f89709f106235a3859ce68f
HauntedHunteer <m.niemczuk@interia.pl>
2022-01-26 20:50:30 +0100

    alert module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AlertModule } from './_alert';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './top-navbar/top-navbar.component';
@@ -17,6 +18,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AlertModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
